035c7104d43d459937f529045c816f033b8b9310
cbetancourt22 <47401691+cbetancourt22@users.noreply.github.com>
2019-12-04 19:37:20 -0800

    Updated the names
    Names are now random for every person

--- person.h
+++ person.h
@@ -1,29 +1,31 @@
 #ifndef PERSON_H
 #define PERSON_H
 #include <string>
+#include <vector>
 #include <iostream>
-//changed
+#include <fstream>
+
 class person
 {
-public:
-	person();
-	person(int sev, std::string name);
-	bool isSick(); //returns if the person is sick
-	bool wasSick(); //returns if the patient was ever sick
-	void getSick(int severity); //makes the person sick with a severity of the passed in interger
-	void cure(); //removes the state of being sick from the person
-	bool operator > (person const &other); //comparing one person to another
-	int getSeverity();
-	std::string getName();
-
 private:
 	std::string name; //holds the persons name
+	std::vector<std::string> residents; //holds the 2000 persons that are in the town of Simville
 	int numVisits; //number of times visiting the ER
 	bool sick; //the person is sick if this is true
 	bool everSick; //the person has been sick if this true
 	int severity; //if not sick == 0 if sick it indicates how sick from 1-20
-	
+
+public:
+	person();
+	person(int sev, std::string name);
+	bool isSick();
+	bool wasSick();
+	void getSick(int severity);
+	void cure();
+	bool operator > (person const &other);
+	int getSeverity();
+	std::string getRandName();
+	void setName();
 };
 
-
-#endif // !PERSON_H
+#endif // PERSON_H
